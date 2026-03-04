28b2b5ca629be9ec47af9f31329009f07e6f74c8
zallendray <kn4jna@gmail.com>
2026-03-03 23:40:05 -0500

    started func to get student data and added structs to main func.

--- Dynamic Memory and Structs/Dynamic Memory and Structs.cpp
+++ Dynamic Memory and Structs/Dynamic Memory and Structs.cpp
@@ -8,20 +8,22 @@ using namespace std;
 
 int get_number_of_students(int& num_students, int& num_grades);
 
+int get_student_data(int num_students, int num_grades, int& Student_List);
 
 int main()
 {
+
 	struct student_info
 	{
 		string name;
 		int student_id;
 		int* grades = nullptr;
 	};
+	student_info* Student_List = nullptr;
 	int num_of_students, num_of_grades;
 	cout << get_number_of_students(num_of_students, num_of_grades);
 	cout << num_of_students;
-
-
+	Student_List = new student_info[num_of_students];
 
 
 }
@@ -49,6 +51,62 @@ int get_number_of_students(int& num_students, int& num_grades)
 					num_grades = temp_int;
 					return 0;
 				}
+			}
+			else {
+				temp += character;
+			}
+		}
+	}
+	else
+	{
+		cout << "ERROR: the specified file does not exist!";
+	}
+
+}
+
+int get_student_data(int num_students, int num_grades, int& Student_List)
+{
+	ifstream inputFile;
+	inputFile.open("student_data.txt");
+	if (inputFile)
+	{
+		char character;
+		string temp;
+		int step = 0, student_number = 0;
+		bool get_data = false;
+
+		while (inputFile.get(character)) 
+		{
+			if (get_data == false) 
+			{
+				if (character == '\n') 
+				{
+					get_data = true;
+				}
+			}
+			else if (character == ' ') 
+			{
+				if (step == 0) 
+				{
+					int temp_int = stoi(temp);
+					num_students = temp_int;
+					temp.clear();
+					step += 1;
+				}
+				else if (step == 1) 
+				{
+
+				}
+				else 
+				{
+					int temp_int = stoi(temp);
+					num_grades = temp_int;
+					return 0;
+				}
+			}
+			else if (character == '\n') 
+			{
+
 			}
 			else {
 				temp += character;
